51482ffbb51db79761f3cb1b33567e3947c98746
Jo Pol <jo-pol@users.noreply.github.com>
2015-10-16 08:17:30 +0200

    simplied titles for start/end nodes, preparations for painting threads

--- show-graph.js
+++ show-graph.js
@@ -1,3 +1,15 @@
+function startPin (svg){
+    svg.append('svg:defs').append('svg:marker')
+        .attr('id', "start-pin")
+        .attr('viewBox', '0 -5 10 10')
+        .attr('markerWidth', 6)
+        .attr('markerHeight', 8 )
+        .attr('orient', 'auto')
+      .append('svg:path')
+        .attr('d', d3.svg.symbol("triangle-down"))
+        .attr('stroke-width', 3)
+        .attr('stroke', "#999");
+}
 function startMarker (svg,id,color){
     svg.append('svg:defs').append('svg:marker')
         .attr('id', id)
@@ -23,6 +35,15 @@ function endMarker (svg,id,color){
         .attr('stroke-width', 3)
         .attr('stroke', color);
 }
+function paintThread(node, links, color) {
+    link = node
+    alert(links[link.next])
+    while (link.next) {
+        link = links[link.next]
+        // TODO paint the link
+        // link.style("stroke",color)
+    }
+}
 function showGraph(container, graph) {
     var width = 800,
         height = 400;
@@ -39,6 +60,7 @@ function showGraph(container, graph) {
 
     force.nodes(graph.nodes).links(graph.links).start();
 
+    startPin(svg)
     startMarker(svg, 'start-green','#0f0')
     endMarker(svg, 'end-green','#0f0')
     startMarker(svg, 'start-red','#f00')
@@ -54,11 +76,16 @@ function showGraph(container, graph) {
     var node = svg.selectAll(".node").data(graph.nodes).enter().append("circle")
         .attr("class", "node")
         .attr("r", 6)
-        .style('opacity', function(d) { return d.text && d.text.match(/[a-zA-Z]/) ? 1: 0})
+        .style('opacity', function(d) { return d.bobbin ? 1: 0})
         .call(force.drag);
 
-    node.append("svg:title").text(function(d) { return d.title ? d.title : d.text; })
-      
+    node.append("svg:title").text(function(d) { return d.title ? d.title : (d.index + 1); })
+
+    paintThread(force.nodes()[0],force.links(),"#f00")
+    paintThread(force.nodes()[1],force.links(),"#0f0")
+    paintThread(force.nodes()[2],force.links(),"#00f")
+    paintThread(force.nodes()[3],force.links(),"#ff0")
+
     force.on("tick", function() {
         link.attr("x1", function(d) { return d.source.x; })
             .attr("y1", function(d) { return d.source.y; })
