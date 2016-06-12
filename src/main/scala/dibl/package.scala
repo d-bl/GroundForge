7c9e4e9384e01b4c1c142678b1a77daab3d71cf5
jo pol <jo-pol@users.noreply.github.com>
2016-06-12 09:50:31 +0200

    scala-doc

--- src/main/scala/dibl/package.scala
+++ src/main/scala/dibl/package.scala
@@ -15,38 +15,62 @@
 */
 
 import scala.annotation.tailrec
-import scala.language.postfixOps
 import scala.collection.immutable.HashMap
+import scala.language.postfixOps
 
 package object dibl {
 
   // got these type aliases from http://stackoverflow.com/questions/15783837/beginner-scala-type-alias-in-scala-2-10
 
+  /** Tuples pointing to another cell in the matrix. */
   type SrcNodes = Array[(Int,Int)]
   def SrcNodes(xs: (Int,Int)*) = Array(xs: _*)
 
+  /** Row in a matrix of tuples, each tuple points to another cell in the matrix. */
   type R = Array[SrcNodes]
   def R(xs: SrcNodes*) = Array(xs: _*)
 
+  /** Matrix of tuples, each tuple points to another cell in the matrix. */
   type M = Array[R]
   def M(xs: R*) = Array(xs: _*)
 
   type TargetToSrcs = (Int, (Int, Int))
   def TargetToSrcs (target: Int, sources: (Int, Int)) = (target, sources)
 
-  /** see https://github.com/d-bl/GroundForge/blob/gh-pages/sample.js */
+  /** see https://github.com/d-bl/GroundForge/blob/gh-pages/js/sample.js */
   type Props = Map[String,Any]
   def Props(xs: (String, Any)*) = HashMap(xs: _*)
-  implicit class RichProps (p: Props) {
-    // link properties
-    def source: Int = p.getOrElse("source",0).asInstanceOf[Int]
-    def target: Int = p.getOrElse("target",0).asInstanceOf[Int]
-    // node properties
+
+  /** Bridges the JavaScript way of accessing object properties like a HashMap
+    * and the scala way allowing code-completion
+    */
+  implicit class LinkProps(p: Props) {
+    /** The id of the source node */
+    def source: Int = p.getOrElse("source", 0).asInstanceOf[Int]
+
+    /** The id of the target node */
+    def target: Int = p.getOrElse("target", 0).asInstanceOf[Int]
+  }
+
+  /** Bridges the JavaScript way of accessing object properties like a HashMap
+    * and the scala way allowing code-completion
+    */
+  implicit class NodeProps(p: Props) {
+
+    /** The title alias tooltip (ID and instructions for a stitch) */
     def title: String = p.getOrElse("title", "").toString
-    def instructions: String = p.title.replaceAll(" .*", "").toLowerCase.replaceAll("t","lr")
-    def startOf: Int = p.getOrElse("startOf","0").toString.replaceAll("thread","").toInt
-    def x: Int = p.getOrElse("x","0").toString.toInt
-    def y: Int = p.getOrElse("y","0").toString.toInt
+
+    /** The stitch instructions from the title */
+    def instructions: String = p.title.replaceAll(" .*", "").toLowerCase.replaceAll("t", "lr")
+
+    /** If none-zero the node the first one of the thread with that number */
+    def startOf: Int = p.getOrElse("startOf", "0").toString.replaceAll("thread", "").toInt
+
+    /** Initial position for the animation */
+    def x: Int = p.getOrElse("x", "0").toString.toInt
+
+    /** Initial position for the animation */
+    def y: Int = p.getOrElse("y", "0").toString.toInt
   }
 
   // other tools
