0cd04949ae80d587d8cb6b12c2ecf890000cd9b2
jo pol <jo-pol@users.noreply.github.com>
2016-10-15 08:51:39 +0200

    do render the first ticks

--- docs/API/index.js
+++ docs/API/index.js
@@ -6,6 +6,7 @@ function load() {
   var shiftLeft = 1
   var shiftUp = 1
   var stitches = 'A3=tctc,D4=lctc,B4=rctc,A1=ctc,A2=tctc,C1=tctc,D2=rctc,B2=lctc,C3=ctc,C4=tctc'
+
   var data = dibl.D3Data().get(matrix, nrOfRows, nrOfCols, shiftLeft, shiftUp, stitches, "checker")
   var patterns = new dibl.PatternSheet(2, "height='210mm' width='297mm'")
   patterns.add(matrix, "checker")
@@ -15,18 +16,11 @@ function load() {
   diagram.showGraph({
     container: '#pairs',
     nodes: data.pairNodes,
-    links: data.pairLinks,
-    scale: 0.6,
-    transform: 0.6,
-    onAnimationEnd: function() { setHref(document.getElementById("dlPair"),'pairs') }
+    links: data.pairLinks
   })
   diagram.showGraph({
     container: '#threads',
     nodes: data.threadNodes,
-    links: data.threadLinks,
-    scale: 1,
-    transform: 1,
-    threadColor: '#color',
-    palette: 1
+    links: data.threadLinks
   })
 }
