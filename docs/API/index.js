f72f2d2dd391ab070a33d9e02f6b9a53296bc0a8
jo pol <jo-pol@users.noreply.github.com>
2016-12-17 18:22:55 +0100

    measure calculation times

--- docs/API/index.js
+++ docs/API/index.js
@@ -5,22 +5,36 @@ function load() {
   var nrOfCols = 9
   var shiftLeft = 1
   var shiftUp = 1
-  var stitches = 'A3=ctc,D4=ctc'
+  var stitches = ''
 
-  var data = dibl.D3Data().get(matrix, nrOfRows, nrOfCols, shiftLeft, shiftUp, stitches, "checker")
   var patterns = new dibl.PatternSheet(2, "height='90mm' width='100mm'")
   patterns.add(matrix, "checker")
   var svg = (patterns.toSvgDoc().trim())
+  var sheetElement = document.getElementById("sheet")
+  sheetElement.innerHTML = svg
 
-  document.getElementById("sheet").innerHTML = svg
+  // TODO force redraw
+
+  var start = new Date().getTime()
+  var data = dibl.D3Data().get(matrix, nrOfRows, nrOfCols, shiftLeft, shiftUp, stitches, "checker")
+  console.log("time for pair diagram " + (new Date().getTime() - start))
+
   diagram.showGraph({
     container: '#pairs',
     nodes: data.pairNodes(),
     links: data.pairLinks()
   })
+
+  // TODO force redraw
+
+  start = new Date().getTime()
+  var nodes = data.threadNodes()
+  var links = data.threadLinks()
+  console.log("time for pair diagram " + (new Date().getTime() - start))
+
   diagram.showGraph({
     container: '#threads',
-    nodes: data.threadNodes,
-    links: data.threadLinks
+    nodes: nodes,
+    links: links
   })
 }
