d-bl/GroundForge

commit f2081625adfae862cf974dbcd25d73b3e836aec7

enhanced API

docs/js/show-graph.js
@@ -152,7 +152,7 @@ diagram.showGraph = function(args) {
     if (colorpicker) {
         threadStarts.on('click', function (d) {
             if (d3.event.defaultPrevented) return
-            sim.alpha(0).stop()
+            diagram.sim.alpha(0).stop()
             svgContainer.selectAll("."+d.startOf)
               .style('stroke', '#'+colorpicker.value)
               .style('fill', function(d) { return d.bobbin ? '#'+colorpicker.value : 'none' })
@@ -194,7 +194,7 @@ diagram.showGraph = function(args) {
                         if (args.onAnimationEnd) args.onAnimationEnd()
                     }
     function strength(link){ return link.weak ? 5 : 50 }
-    var sim = d3.forceSimulation(args.nodes)
+    diagram.sim = d3.forceSimulation(args.nodes)
         .force("charge", d3.forceManyBody().strength(-1000))
         .force("link", d3.forceLink(args.links).strength(strength).distance(12).iterations(30))
         .force("center", d3.forceCenter(args.viewWidth / 2, args.viewHeight / 2))
@@ -216,7 +216,7 @@ diagram.showGraph = function(args) {
                    .on("drag", dragged)
                    .on("end", dragended))
     function dragstarted(d) {
-      if (!d3.event.active) sim.alpha(0.005).restart()
+      if (!d3.event.active) diagram.sim.alpha(0.005).restart()
       d.fx = d.x;
       d.fy = d.y;
     }
@@ -226,7 +226,7 @@ diagram.showGraph = function(args) {
     }
     function dragended(d) {
       step = 0
-      if (!d3.event.active) sim.alpha(0.005).restart()
+      if (!d3.event.active) diagram.sim.alpha(0.005).restart()
       d.fx = null;
       d.fy = null;
     }

docs/js/batch.js
@@ -16,47 +16,58 @@
 /******************************************************************
  install:      node.js
  execute:      nmp install jsdom
- dos terminal: NODE_PATH=C:\???\docs\js;/C:\???\node_modules
- shell:        export NODE_PATH=/???/docs/js;/???/node_modules
+ 
+ dos terminal: set NODE_PATH="C:\???\docs\js;/C:\???\node_modules"
+ shell:        export NODE_PATH="/???/docs/js;/???/node_modules"
 
- execute:      node -e 'require("batch.js");svgFile="tmp.svg"'
+ execute:      node -i -e 'require("batch.js");svgFile="tmp.svg"'            
+               > createSVG("ct;ctc","#000,#000,#f00,#f00",1)
+               > .exit
 *******************************************************************/
 
-// from GroundForge/docs/js
+// from docs/js
 require("matrix-graphs.js")
 require("show-graph.js")
 d3 = require("d3.v4.min.js")
 // from node_modules
 fs = require("fs")
-document = require("jsdom").jsdom()
 
+document = require("jsdom").jsdom()
 navigator = {}
-function createSVG (colors) {
+
+createSVG = function (stitches, colors, countDown) {
+  stitches.split(";").forEach(function(s){
+    console.log("applying " + s + " to " + d3data.threadNodes().length + " nodes")
+    d3data = dibl.D3Data().get(s, d3data)
+  })
+  console.log("created " + d3data.threadNodes().length + " nodes")
+  document.body.innerHTML = ""
   diagram.showGraph({
     container: document.body,
-    nodes: finalStep.threadNodes(),
-    links: finalStep.threadLinks(),
+    nodes: d3data.threadNodes(),
+    links: d3data.threadLinks(),
     viewWidth: 744,
     viewHeight: 1052,
     palette: colors,
     onAnimationEnd: function() {
-      fs.writeFile(svgFile, document.body.innerHTML, function(err) {
+      if (--countDown > 0) {
+          console.log("nudging animation " + countDown)
+          diagram.sim.alpha(0.005).restart()
+      } else fs.writeFile(svgFile, document.body.innerHTML, function(err) {
         if(err) return console.log(err)
-        else console.log(svgFile + " was saved!")
+        else console.log(svgFile + " was saved")
       })
     }
   })
 }
 
-// initial parameters
-var matrix = "5-"
-var tiling = "bricks"
-var nrOfRows = 9
-var nrOfCols = 9
-var shiftLeft = 0
-var shiftUp = 1
-
-// calculate the patterns step by step
-var step1 = dibl.D3Data().get(matrix, nrOfRows, nrOfCols, shiftLeft, shiftUp, "ctct", tiling)
-var finalStep = dibl.D3Data().get("ctct", step1)
-createSVG("#000,#000,#F00,#F00")
+// the initial pattern
+d3data = dibl.D3Data().get(
+    matrix = "5-",
+    nrOfRows = 5,
+    nrOfCols = 5,
+    shiftLeft = 0,
+    shiftUp = 1,
+    stitches = "ctct",
+    tiling = "bricks"
+)

commit 4078ea2c4757fd53cf68e46c65c22acd37318741

fixed dragging nodes on pair diagrams, broken by f208162

docs/js/show-graph.js
@@ -87,7 +87,7 @@ diagram.showGraph = function(args) {
     if (colorpicker) {
         threadStarts.on('click', function (d) {
             if (d3.event.defaultPrevented) return
-            diagram.sim.alpha(0).stop()
+            sim.alpha(0).stop()
             svgContainer.selectAll("."+d.startOf)
               .style('stroke', '#'+colorpicker.value)
               .style('fill', function(d) { return d.bobbin ? '#'+colorpicker.value : 'none' })
@@ -133,7 +133,7 @@ diagram.showGraph = function(args) {
                         if (args.onAnimationEnd) args.onAnimationEnd()
                     }
     function strength(link){ return link.weak ? 5 : 50 }
-    diagram.sim = d3.forceSimulation(args.nodes)
+    var sim = d3.forceSimulation(args.nodes)
         .force("charge", d3.forceManyBody().strength(-1000))
         .force("link", d3.forceLink(args.links).strength(strength).distance(12).iterations(30))
         .force("center", d3.forceCenter(args.viewWidth / 2, args.viewHeight / 2))
@@ -155,7 +155,7 @@ diagram.showGraph = function(args) {
                    .on("drag", dragged)
                    .on("end", dragended))
     function dragstarted(d) {
-      if (!d3.event.active) diagram.sim.alpha(0.005).restart()
+      if (!d3.event.active) sim.alpha(0.005).restart()
       d.fx = d.x;
       d.fy = d.y;
     }
@@ -165,7 +165,7 @@ diagram.showGraph = function(args) {
     }
     function dragended(d) {
       step = 0
-      if (!d3.event.active) diagram.sim.alpha(0.005).restart()
+      if (!d3.event.active) sim.alpha(0.005).restart()
       d.fx = null;
       d.fy = null;
     }