f7d3b8578304b831264c6b555a59d7819b12cdd1
jo pol <jo-pol@users.noreply.github.com>
2017-04-19 12:15:53 +0200

    fixed #80 patternsheets on pages of their own

---
+++ docs/js/sheet.js
@@ -0,0 +1,20 @@
+function load() {
+  var nr = 1
+  var location = (window.location.href + "").replace("#","")
+  var container = document.getElementById("patterns")
+  var multiSheet = new dibl.PatternSheet(3, "width='210mm' height='297mm'")
+
+  // for each key-value pair in the URL query
+  location.replace(/[?&]+([^=&]+)(=([^&]*))?/gi, function(m,key,m2,value) {
+    var values = decodeURIComponent(value).replace(/[+]/g, " ").split(";")
+    if (values.length == 2) {
+      var singleSheet = new dibl.PatternSheet(2, "height='90mm' width='100mm'", "PATTERN" + nr++)
+      multiSheet.add(values[0], values[1])
+      singleSheet.add(values[0], values[1])
+      container.innerHTML += (singleSheet.toSvgDoc().trim())
+    }
+  })
+  document.getElementById("download").href = 'data:image/svg+xml,' +
+   encodeURIComponent('<!--?xml version="1.0" encoding="UTF-8" standalone="no"?-->' +
+    multiSheet.toSvgDoc().trim())
+}
