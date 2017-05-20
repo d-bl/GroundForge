9254a7947472c8146d58d90364505cb184f5725b
jo pol <jo-pol@users.noreply.github.com>
2017-05-20 20:21:31 +0200

    simplified unit tests for failing matrices
    (cherry picked from commit bb22cac)

--- src/test/scala/dibl/GallerySpec.scala
+++ src/test/scala/dibl/GallerySpec.scala
@@ -2,7 +2,7 @@ package dibl
 
 import org.scalatest.{FlatSpec, Matchers}
 
-import scala.util.{Failure, Success}
+import scala.util.Success
 
 class GallerySpec extends FlatSpec with Matchers {
   "each gallery pattern" should "succeed (one fails because of bug #93)" in {
@@ -17,43 +17,42 @@ class GallerySpec extends FlatSpec with Matchers {
   }
 
   "some patterns" should "document bug #93 by failing" in {
-    // discovered by @MAETempels
-    PairDiagram.create("5--- -C-B --5- -B-C", "checker", absRows = 9, absCols = 9) shouldBe a[Failure[_]]
-    PairDiagram.create("5831,-4-7,--5-,-B-C", "checker", absRows = 9, absCols = 9) shouldBe a[Failure[_]]
-    PairDiagram.create("5831,-4-7,--5-,-B-C,3158,-7-4,5---,-C-B", "checker", absRows = 9, absCols = 9) shouldBe a[Failure[_]]
-    PairDiagram.create("6--4,-C-4,--58,-B68,-46-,-4-C,58--,68-B", "checker", absRows = 9, absCols = 9) shouldBe a[Failure[_]]
-
-    // from Matrices.values
-    PairDiagram.create("---5 C-B- -5-- B-C-", "checker", absRows = 9, absCols = 9) shouldBe a[Failure[_]]
-    PairDiagram.create("C3B- 256- ---5", "checker", absRows = 9, absCols = 9) shouldBe a[Failure[_]]
-    PairDiagram.create("---5 C3B- 256-", "checker", absRows = 9, absCols = 9) shouldBe a[Failure[_]]
-    PairDiagram.create("256- ---5 C3B-", "checker", absRows = 9, absCols = 9) shouldBe a[Failure[_]]
-    PairDiagram.create("256- ---5 C3B-", "bricks", absRows = 9, absCols = 9, shiftUp = 5) shouldBe a[Failure[_]]
     Seq(
-      "6868 ---4 2AA1 -7-7;bricks",
-      "6868 ---- AAAA -7-7;bricks",
-      "586- ---5 2AA1 -7-7;bricks",
-      "-4-4 5--- -C-B 6868;bricks",
-      "-4-5 5--- -C-B 6-58;bricks",
-      "B-C- ---5 C-B- -5--;checker",
-      "5-5- -5-- B-C- -5-5;bricks",
-      "-4-7 5--- -C-B 3158;bricks",
-      "586- ---5 2AB- -7-5;bricks",
-      "6868 ---4 2AA1 -7-7;bricks",
-      "6868 ---7 AA01 -7-7;bricks",
-      "6868 ---- AAAA -7-7;bricks",
-      "586- ---5 2AA1 -7-7;bricks",
-      "-4-4 5--- -C-B 6868;bricks",
-      "-4-5 5--- -C-B 6-58;bricks"
-    ).map { l =>
-      val (matrix, tiling) = l.split(";") match{case Array(m,t) => (m,t)}
-      PairDiagram.create(matrix, tiling, absRows = 9, absCols = 9, shiftUp=3).isFailure
+      // discovered by @MAETempels
+      ("5---,-C-B,--5-,-B-C", "checker", 0),
+      ("5831,-4-7,--5-,-B-C", "checker", 0),
+      ("5831,-4-7,--5-,-B-C,3158,-7-4,5---,-C-B", "checker", 0),
+      ("6--4,-C-4,--58,-B68,-46-,-4-C,58--,68-B", "checker", 0),
+      // from Matrices.values, in other words: from docs/gallery.html
+      ("---5,C-B-,-5--,B-C-", "checker", 0),
+      ("---5,C3B-,256-", "checker", 0),
+      ("256-,---5,C3B-", "checker", 0),
+      ("C3B-,256-,---5", "checker", 0),
+      ("C3B-,6-25,-5--", "bricks", 0),
+      ("B-C3,256-,---5", "bricks", 0),
+      ("6868,---4,2AA1,-7-7","bricks", 3),
+      ("6868,----,AAAA,-7-7","bricks", 3),
+      ("586-,---5,2AA1,-7-7","bricks", 3),
+      ("-4-4,5---,-C-B,6868","bricks", 3),
+      ("-4-5,5---,-C-B,6-58","bricks", 3),
+      ("B-C-,---5,C-B-,-5--","checker", 3),
+      ("5-5-,-5--,B-C-,-5-5","bricks", 3),
+      ("-4-7,5---,-C-B,3158","bricks", 3),
+      ("586-,---5,2AB-,-7-5","bricks", 3),
+      ("6868,---4,2AA1,-7-7","bricks", 3),
+      ("6868,---7,AA01,-7-7","bricks", 3),
+      ("6868,----,AAAA,-7-7","bricks", 3),
+      ("586-,---5,2AA1,-7-7","bricks", 3),
+      ("-4-4,5---,-C-B,6868","bricks", 3),
+      ("-4-5,5---,-C-B,6-58","bricks", 3)
+    ).map{ case (matrix, tiling, shift) =>
+      PairDiagram.create(matrix, tiling, absRows = 9, absCols = 9, shiftUp = shift).isFailure
     } should contain only true
   }
 
   it should "work for some set of parameters" in {
-    PairDiagram.create("B-B- -B-B C-C- -C-C", "bricks", absRows = 9, absCols = 5) shouldBe a[Success[_]]
-    tryConfigurations("B-B- -B-B C-C- -C-C", "bricks").mkString shouldNot include("true")
+    PairDiagram.create("B-B-,-B-B,C-C-,-C-C", "bricks", absRows = 9, absCols = 5) shouldBe a[Success[_]]
+    tryConfigurations("B-B-,-B-B,C-C-,-C-C", "bricks").mkString shouldNot include("true")
   }
 
   private def tryConfigurations(matrix: String, tiling: String) = {
