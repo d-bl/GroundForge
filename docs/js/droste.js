59c6b76a34e369b34f4a182b6938e5c080834a55
jo pol <jo-pol@users.noreply.github.com>
2017-08-06 14:56:33 +0200

    work in progress

---
+++ docs/js/droste.js
@@ -0,0 +1,22 @@
+function setVisibility() {
+
+  var values = document.getElementById("tileDimension").value.split("x")
+  var diagonal = document.getElementById("matrixDiagonal").checked
+  var height = values[0]
+  var width = values[1]
+  for (c = 0; c < 8; c++) {
+    for (r = 0; r < 4; r++) {
+//        setEnabled(
+//          "r"+(r+1)"-c"+(c+1),
+//          c >= width && r >= height && (!diagonal || c%2 == r%2)
+//          )
+    }
+  }
+}
+function setEnabled(id, enabled) {
+  var x = document.getElementById(id)
+  x.className = (enabled
+                ? x.className.replace("hide","show")
+                : x.className.replace("show","hide")
+                )
+}
