2037e9d080eddfb9511096b4c556f20b23cf22da
Joke Pol <joke.pol@dans.knaw.nl>
2017-08-06 16:55:49 +0200

    should work more or less

--- docs/js/droste.js
+++ docs/js/droste.js
@@ -1,17 +1,26 @@
 function setVisibility() {
 
-  var diagonal = document.getElementById("matrixDiagonal").checked
+  var diagonal = document.getElementById("matrix").value == "5-"
   var values = document.getElementById("tileDimension").value.split("x")
   var height = values[0]
   var width = values[1]
+  var stitches = ""
   for (c = 0; c < 8; c++) {
     for (r = 0; r < 4; r++) {
-        setEnabled(
-          "r"+(r+1)+"-c"+(c+1),
-          c < width && r < height && (!diagonal || c%2 == r%2)
-        )
+      var enable =  c < width && r < height && (!diagonal || c%2 == r%2)
+      var id = "r" + (r + 1) + "-c" + (c + 1)
+      setEnabled(id, enable)
+      if (enable)
+        stitches += document.getElementById(id).name
+            + "=" + document.getElementById(id).value + " "
     }
   }
+  document.getElementById("link").href = "index.html?m="
+  + document.getElementById("matrix").value
+  + ";" + document.getElementById("tiling").value
+  + ";8;8;0;0"
+  + "&s1=" + stitches
+  + "&s2=&s3=&"
 }
 function setEnabled(id, enabled) {
   var x = document.getElementById(id)
