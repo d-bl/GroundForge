5eca1fc9967691df3e6fd9c9fd48615e0c916716
Joke Pol <joke.pol@dans.knaw.nl>
2017-08-06 16:16:10 +0200

    dis/enabled stitch cells

--- docs/js/droste.js
+++ docs/js/droste.js
@@ -1,15 +1,15 @@
 function setVisibility() {
 
-  var values = document.getElementById("tileDimension").value.split("x")
   var diagonal = document.getElementById("matrixDiagonal").checked
+  var values = document.getElementById("tileDimension").value.split("x")
   var height = values[0]
   var width = values[1]
   for (c = 0; c < 8; c++) {
     for (r = 0; r < 4; r++) {
-//        setEnabled(
-//          "r"+(r+1)"-c"+(c+1),
-//          c >= width && r >= height && (!diagonal || c%2 == r%2)
-//          )
+        setEnabled(
+          "r"+(r+1)+"-c"+(c+1),
+          c < width && r < height && (!diagonal || c%2 == r%2)
+        )
     }
   }
 }
@@ -19,4 +19,5 @@ function setEnabled(id, enabled) {
                 ? x.className.replace("hide","show")
                 : x.className.replace("show","hide")
                 )
+  x.disabled = !enabled
 }
