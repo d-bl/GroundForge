31626d2c5340b09d9a487b45924babc0388be3e8
jo pol <jo-pol@users.noreply.github.com>
2017-11-11 08:50:51 +0100

    prototype diagram of new form completed

--- docs/js/tiles.js
+++ docs/js/tiles.js
@@ -7,32 +7,27 @@ function setVisibility() {
   var stitches = ""
   var r = 0
   var c = 0
-  var noShift = document.getElementById('shift').value == 0
+  var shift = document.getElementById('shift').value * 1
+  var shiftCols = document.getElementById('cols').checked
   for (r = 0; r < 8; r++) {
-    cols = Math.min(8,matrixLines[r%rows].length)
+    var cols = Math.min(8,matrixLines[r%rows].length)
     for (c = 0; c < 8; c++) {
       var enable =  r < rows && c < cols && matrixLines[r][c] != "-"
       var id = "r" + (r + 1) + "-c" + (c + 1)
       setEnabled(id, enable)
-      var svgId = "svgR" + (r + 1) + "-C" + (c + 1)
-      var el = document.getElementById(svgId)
-      var href = el.attributes["xlink:href"]
-      if (enable) {
-        href.value = "#g" + matrixLines[r][c]
-        el.style = "opacity:1.0;"
-      } else if (noShift) {
-        href.value = "#g" + matrixLines[r%rows][c%cols]
-        el.style = "opacity:0.4;"
-      } else {
-        href.value = "#g-"
-        el.style = "opacity:0.4;"
-      }
+      var svgEl = document.getElementById("svgR" + (r + 1) + "-C" + (c + 1))
+      var firstTile = r < rows && c < cols
+      var colOffset = (!shiftCols ? 0 : Math.floor(r/rows) * shift)
+      var rowOffset = (shiftCols ? 0 : Math.floor(c/cols) * shift)
+      var hrefValue = matrixLines[(r+rowOffset)%rows][(c+colOffset)%cols]
+      svgEl.style = "opacity:" + (!firstTile || hrefValue == "-" ? "0.3;" : "1;")
+      svgEl.attributes["xlink:href"].value = "#g" + hrefValue
     }
   }
   // TODO document.getElementById("diagram").innerHTML += "generating diagram"
   document.getElementById("link").href = "javascript:alert('not yet implemented')"
   document.getElementById('patternByName')[0].selected = "selected"
- }
+    }
 function setEnabled(id, enabled) {
   var x = document.getElementById(id)
   x.className = (enabled
