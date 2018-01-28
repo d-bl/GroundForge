74d488e74c9394e40919f5b3169f2ff569e345eb
jo pol <jo-pol@users.noreply.github.com>
2018-01-28 19:22:18 +0100

    Error marks built into the prototype.
    Signed-off-by: jo pol <jo-pol@users.noreply.github.com>

--- src/main/scala/dibl/InteractiveSVG.scala
+++ src/main/scala/dibl/InteractiveSVG.scala
@@ -20,6 +20,9 @@ object InteractiveSVG {
   @JSExport
   def create(config: Config): String = {
     val itemMatrix = config.itemMatrix
+    val pairsOut: Array[Array[Int]] = computePairsOut(config)
+
+    pairsOut.head
     (for {
       r <- itemMatrix.indices
       c <- itemMatrix.head.indices
@@ -28,13 +31,40 @@ object InteractiveSVG {
       val color = defaultColorValue(stitch)
       val vectorCode = itemMatrix(r)(c).vectorCode.toString.toUpperCase
       val opacity = if (vectorCode == "-") "0.05" else if (itemMatrix(r)(c).isOpaque) "1" else "0.3"
-      s"""<use xlink:href='#g${ vectorCode }'
-         |  id='svg-r${r+1}-c${c+1}'
-         |  transform='translate(${c*10+38},${r*10+1})'
-         |  style='stroke:${if (color.nonEmpty) color else "#000"};opacity:$opacity;"'
-         |  onclick='${if (opacity=="1") "setStitch(this)" else ""}'
-         |><title>$stitch</title>
-         |</use>""".stripMargin
+      val translate = s"transform='translate(${c * 10 + 38},${r * 10 + 1})'"
+      val nrOfPairsOut = pairsOut(r)(c)
+      (if (nrOfPairsOut == 0 || nrOfPairsOut == 2) ""
+      else s"""<use xlink:href='#oops' $translate style='opacity:0.${1 + nrOfPairsOut};'/>"""
+        ) +
+        s"""<use xlink:href='#g$vectorCode'
+           |  id='svg-r${r + 1}-c${c + 1}'
+           |  $translate
+           |  style='stroke:${if (color.nonEmpty) color else "#000"};opacity:$opacity;'
+           |  onclick='${if (opacity == "1") "setStitch(this)" else ""}'
+           |><title>$stitch</title>
+           |</use>""".stripMargin
     }).mkString("\n")
+  }
+
+  private def computePairsOut(config: Config) = {
+    val itemMatrix = config.itemMatrix
+    val rows: Int = itemMatrix.length
+    val cols: Int = itemMatrix.head.length
+    val pairsOut = Array.fill[Array[Int]](rows)(
+      Array.fill[Int](cols)(0)
+    )
+    for {r <- 0 until rows
+         c <- 0 until cols
+    } {
+      Matrix.charToRelativeTuples(itemMatrix(r)(c).vectorCode.toUpper)
+        .foreach { case (relativeSourceRow, relativeSourceCol) =>
+          val row: Int = r + relativeSourceRow
+          val col: Int = c + relativeSourceCol
+          if (row >= 0 && col >= 0 && col < cols && row < rows) {
+            pairsOut(row)(col) += 1
+          }
+        }
+    }
+    pairsOut
   }
 }
