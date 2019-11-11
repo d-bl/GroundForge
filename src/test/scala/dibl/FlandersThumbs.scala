9cd1376f9b7c2dc9c385919e1cca7afd4e90ca4b
Jo Pol <jo-pol@users.noreply.github.com>
2019-11-11 21:13:37 +0100

    Symmetric Flanders permutations

---
+++ src/test/scala/dibl/FlandersThumbs.scala
@@ -0,0 +1,58 @@
+/*
+ Copyright 2016 Jo Pol
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see http://www.gnu.org/licenses/gpl.html dibl
+*/
+package dibl
+
+import dibl.D3jsSVG.{prolog, render}
+import dibl.Force.Point
+import dibl.proto.TilesConfig
+
+import scala.reflect.io.File
+
+object FlandersThumbs extends DemoFixture {
+  def main(args: Array[String]): Unit = {
+    val stitches = Seq("ct", "cl", "cr", "ctc", "clc", "crc", "ctct", "clct", "crct", "ctcl", "clcl", "crcl", "ctcr", "clcr", "crcr")
+    val symmetricStitches = Seq("ct", "ctc", "ctct")
+    val patterns = for {
+      a1 <- symmetricStitches
+      b1 <- stitches
+      c1 = flip(a1)
+      a2 <- stitches
+      c2 = flip(a2)
+      b3 <- symmetricStitches
+    } yield (s"$testDir/$a1-$b1-$c1-$a2-$c2-$b3.svg",
+      s"a1=$a1&b1=$b1&c1=$c1&a2=$a2&c2=$c2&b3=$b3&" +
+        "tile=831,4-7,-5-&patchWidth=9&patchHeight=10&" +
+        "shiftColsSW=-2&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2")
+    val filtered = patterns // 50625/2025 with all/symmetric stitches for B1/B3
+      .filter{case (_,q)=> q.contains("=ct&")} // 1241
+      .filter{case (_,q)=> q.contains("b1=ctc") || q.contains("b3=ctc") } // 746
+    println(filtered.size)
+    filtered.foreach { case (fileName, q) =>
+      Force.nudgeNodes(
+        ThreadDiagram(NewPairDiagram.create(TilesConfig(q))),
+        Point(100, 100)
+      ).map(diagram => File(fileName).writeAll(prolog + render(diagram, width = 400, height = 400)))
+    }
+    System.exit(0)
+  }
+
+  private def flip(a: String) = {
+    a.mkString("")
+      .replaceAll("l", "R")
+      .replaceAll("r", "L")
+      .toLowerCase()
+  }
+}
