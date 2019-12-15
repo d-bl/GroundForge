6de578e4ee6e88d0c95bf64f118ee957a7660d8b
Jo Pol <jo-pol@users.noreply.github.com>
2019-12-15 09:14:37 +0100

    test each pattern with different stitches

--- src/test/scala/fte/Demo.scala
+++ src/test/scala/fte/Demo.scala
@@ -27,23 +27,32 @@ object Demo {
     val dir = new File("target/test/fte-demo")
     dir.mkdirs()
     dir.listFiles().foreach(_.delete())
-    Seq(
-      "bandage&tileStitch=ctc&patchWidth=3&patchHeight=4&tile=1,8&tileStitch=ctc&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=1&shiftRowsSE=2",
-      "sheered&tileStitch=ct&patchWidth=6&patchHeight=4&tile=l-,-h&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2",
-      // the patterns above even fail as pair diagrams, increasing the patch size doesn't help
-      "torchon&tileStitch=ctc&patchWidth=6&patchHeight=4&tile=5-,-5&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2",
-      "rose&tileStitch=ct&patchWidth=12&patchHeight=4&tile=5831,-4-7&shiftColsSW=-2&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2",
-      "pinwheel&tileStitch=ct&patchWidth=12&patchHeight=8&tile=586-,-4-5,5-21,-5-7&shiftColsSE=4&shiftRowsSE=4&shiftColsSW=0&shiftRowsSW=4&",
-      "whiting=F14_P193&tileStitch=ct&patchWidth=24&patchHeight=28&tile=-XX-XX-5,C-X-X-B-,-C---B-5,5-C-B-5-,-5X-X5-5,5XX-XX5-,-XX-XX-5,C-----B-,-CD-AB--,A11588D-,-78-14--,A11588D-,-78-14--,A11588D-&shiftColsSW=0&shiftRowsSW=14&shiftColsSE=8&shiftRowsSE=14",
-      // for now prefixed id's with X in the next pattern to just apply the tileStitch everywhere
-      "braid&patchWidth=18&tileStitch=ctct&patchHeight=8&tile=-B-C-y,B---cx,xC-B-x,m-5-b-&shiftColsSW=0&shiftRowsSW=4&shiftColsSE=6&shiftRowsSE=4&Xa4=llcttct&Xe4=rrcttctrr",
-    ).zipWithIndex.foreach { case (query, i) =>
+    for {
+      stitch <- Seq("ct", "ctc", "ctct", "crcrctclclcr")
+      query <- Seq(
+        s"bandage&tileStitch=$stitch&patchWidth=3&patchHeight=4&tile=1,8&tileStitch=ctc&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=1&shiftRowsSE=2",
+        s"sheered&tileStitch=$stitch&patchWidth=6&patchHeight=4&tile=l-,-h&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2",
+        // the patterns above even fail as pair diagrams, increasing the patch size doesn't help
+        s"torchon&tileStitch=$stitch&patchWidth=6&patchHeight=4&tile=5-,-5&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2",
+        s"rose&tileStitch=$stitch&patchWidth=12&patchHeight=4&tile=5831,-4-7&shiftColsSW=-2&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2",
+        s"pinwheel&tileStitch=$stitch&patchWidth=12&patchHeight=8&tile=586-,-4-5,5-21,-5-7&shiftColsSE=4&shiftRowsSE=4&shiftColsSW=0&shiftRowsSW=4&",
+        s"whiting=F14_P193&tileStitch=$stitch&patchWidth=24&patchHeight=28&tile=-XX-XX-5,C-X-X-B-,-C---B-5,5-C-B-5-,-5X-X5-5,5XX-XX5-,-XX-XX-5,C-----B-,-CD-AB--,A11588D-,-78-14--,A11588D-,-78-14--,A11588D-&shiftColsSW=0&shiftRowsSW=14&shiftColsSE=8&shiftRowsSE=14",
+        // for now prefixed id's with X in the next pattern to just apply the tileStitch everywhere
+        s"braid&patchWidth=18&tileStitch=$stitch&patchHeight=8&tile=-B-C-y,B---cx,xC-B-x,m-5-b-&shiftColsSW=0&shiftRowsSW=4&shiftColsSE=6&shiftRowsSE=4&Xa4=llcttct&Xe4=rrcttctrr",
+      )
+    } {
       Try(GraphCreator.fromDiagram(query)) match {
-        case Success(None) => println(s"$i has no solution")
+        case Success(None) =>
         case Failure(e) => e.printStackTrace()
         case Success(Some(graph)) =>
-          new SVGRender().draw(graph, s"$dir/$i.svg")
+          new SVGRender().draw(graph, s"$dir/${qName(query)}-$stitch.svg")
       }
     }
+  }
+
+  private def qName(query: String) = {
+    query
+      .replaceAll("&.*", "")
+      .replaceAll("[^a-zA-Z0-9]+", "-")
   }
 }
