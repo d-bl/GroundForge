f697ca24867974eb53f3778dbefc64885a2ae6f7
jo-pol <jo-pol@users.noreply.github.com>
2019-12-26 17:39:00 +0100

    extract methods

--- src/main/scala/dibl/TopoLink.scala
+++ src/main/scala/dibl/TopoLink.scala
@@ -18,9 +18,9 @@ object TopoLink {
     implicit val complexLinks: Seq[LinkProps] = linksInOneTile
     linksInOneTile.map(link => {
       isLeftOfSource(link)
-      // TODO so far attempts to fix isLeftOfSource caused eternal loops,
+      // TODO so far fixing isLeftOfSource causes exceptions,
       //  it should simplify Vertex.addEdge and GraphCreator.deltas
-      TopoLink(sourceIdOf(link), targetIdOf(link), isLeftOfTarget(link), isLeftOfSource = isLeftOfTarget(link))
+      TopoLink(sourceOf(link).id, targetOf(link).id, isLeftOfTarget(link), isLeftOfSource = isLeftOfTarget(link))
     })
   }
 
@@ -28,36 +28,55 @@ object TopoLink {
                             (implicit diagram: Diagram,
                              linksInTile: Seq[LinkProps]
                             ): Boolean = {
-    (
-      diagram.node(link.target).instructions,
-      link.isInstanceOf[WhiteStart]
-    ) match {
-      /*    cross   twist
-       *    \  /    \   /
-       *     \        /
-       *    / \     /  \
-       */
-      case ("cross", true) => true
-      case ("cross", false) => false
-      case ("twist", false) => true
-      case ("twist", true) => false
-      case _ => // TODO relatively expensive lookup for large matrices
-        val otherX: Double = linksInTile.find(other =>
-          sourceIdOf(other) != sourceIdOf(link) &&
-            targetIdOf(other) == targetIdOf(link)
-        ).map(other => diagram.node(other.source).x).getOrElse(0)
-        diagram.node(link.source).x < otherX
+    def isSiblingAtTarget(other: LinkProps) = {
+      sourceOf(other).id != sourceOf(link).id &&
+        targetOf(other).id == targetOf(link).id
     }
+
+    def isLeftPairOfTarget = {
+      val otherX: Double = linksInTile
+        .find(isSiblingAtTarget) // TODO relatively expensive lookup for large matrices
+        .map(other => sourceOf(other).x)
+        .getOrElse(0)
+      sourceOf(link).x < otherX
+    }
+
+    isLeft(
+      targetOf(link).instructions,
+      link.isInstanceOf[WhiteStart],
+      isLeftPairOfTarget _
+    )
   }
 
   private def isLeftOfSource(link: LinkProps)
                             (implicit diagram: Diagram,
                              linksInTile: Seq[LinkProps]
                             ): Boolean = {
-    (
-      diagram.node(link.source).instructions,
-      link.isInstanceOf[WhiteStart]
-    ) match {
+    def isSiblingAtSource(other: LinkProps) = {
+      targetOf(other).id != targetOf(link).id &&
+        sourceOf(other).id == sourceOf(link).id
+    }
+
+    def isLeftPairAtSource = {
+      val otherX: Double = linksInTile
+        .find(isSiblingAtSource) // TODO relatively expensive lookup for large matrices
+        .map(other => targetOf(other).x)
+        .getOrElse(0)
+      targetOf(link).x < otherX
+    }
+
+    isLeft(
+      sourceOf(link).instructions,
+      link.isInstanceOf[WhiteStart],
+      isLeftPairAtSource _
+    )
+  }
+
+  private def isLeft(sourceInstructions: String,
+                     isWhiteStart: Boolean,
+                     isLeftPair: () => Boolean
+                    ) = {
+    (sourceInstructions, isWhiteStart) match {
       /*    cross   twist
        *    \  /    \   /
        *     \        /
@@ -67,22 +86,17 @@ object TopoLink {
       case ("cross", false) => false
       case ("twist", false) => true
       case ("twist", true) => false
-      case _ => // TODO relatively expensive lookup for large matrices
-        val otherX: Double = linksInTile.find(other =>
-          sourceIdOf(other) == sourceIdOf(link) &&
-            targetIdOf(other) != targetIdOf(link)
-        ).map(other => diagram.node(other.target).x).getOrElse(0)
-        diagram.node(link.target).x < otherX
+      case _ => isLeftPair()
     }
   }
 
-  private def sourceIdOf(l: LinkProps)
-                        (implicit diagram: Diagram) = {
-    diagram.node(l.source).id
+  private def sourceOf(l: LinkProps)
+                      (implicit diagram: Diagram) = {
+    diagram.node(l.source)
   }
 
-  private def targetIdOf(l: LinkProps)
-                        (implicit diagram: Diagram) = {
-    diagram.node(l.target).id
+  private def targetOf(l: LinkProps)
+                      (implicit diagram: Diagram) = {
+    diagram.node(l.target)
   }
 }
