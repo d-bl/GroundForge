1e64fb59cb4b3431a78179645db7652e1f4bd7f5
Jo Pol <jo-pol@users.noreply.github.com>
2021-10-04 20:58:55 +0200

    reverted

--- docs/js/nets.js
+++ docs/js/nets.js
@@ -1,56 +1,103 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title lang="en">GroundForge: nets</title>
-    <style>
-        figure { display: inline-block; margin: 10px; vertical-align: top; }
-        .intro { width: 37em; }
-        pre { display: inline-block; margin: 0 0.3em 0 0.3em; }
-        td { border: solid 1px; padding: 0.3em; }
-        table { margin-left: 2.5em; border-collapse: collapse; }
-        span { color: red }
-    </style>
-    <script src="js/d3.v4.min.js" type="text/javascript"></script>
-    <script src="js/GroundForge-opt.js" type="text/javascript"></script>
-    <script src="js/nets.js" type="text/javascript"></script>
-    <link rel="icon" type="image/png" sizes="32x32" href="/GroundForge/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="/GroundForge/favicon/favicon-16x16.png">
-</head>
-<body onload="load()">
-<div class="intro">
-    <p>
-        Examples of the
-        <a href="https://d-bl.github.io/MAE-gf/docs/counting#results" target="_blank"
-        >words</a> you can put after "<code>?b=</code>" at the end of the current page adress.
-    </p>
-    <p>
-        Follow the links to:
-    </p>
-    <ul>
-        <li>
-            tweak the patterns such as apply colors to threads
-        </li>
-        <li>
-            try another "<code>dbpq</code>" configuration:
-            adjust the address of the followed link as suggested by the table.
-            Providing all "<code>dbpq</code>" combinations would make this page too slow.
-        </li>
-    </ul>
-<table>
-    <tr><td>stitch<br>arrangement<br>x = d, p or q</td><td>page address contains</td><td>remarks</td></tr>
-    <tr><td><pre>bb
-xx</pre></td><td><code>b1=b&c1=b&b2=x&c2=x</code></td><td>provided</td></tr>
-    <tr><td><pre>bx
-bx</pre></td><td><code>b1=b&<span>b2</span>=b&<span>c1</span>=x&c2=x</code></td><td rowspan="2">DIY, changed parts in red</td></tr>
-    <tr><td><pre>bx
-xb</pre></td><td><code>b1=b&<span>c2</span>=b&b2=x&<span>c1</span>=x</code></td></tr>
-    <tr><td><pre>bp
-qd</pre></td><td><code>b1=b&c2=q&c1=p&b2=d</code></td><td>provided</td></tr>
-    <tr><td colspan="3">stitch labels:<br><pre>b1,c1
-b2,c2</pre></td></tr>
-</table>
-</div>
-<div id="diagrams"></div>
-</body>
-</html>
+/*
+ Copyright 2022 Jo Pol
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see http://www.gnu.org/licenses/gpl.html dibl
+*/
+function load() {
+  // TODO allow 4 stitches: ?b=..&d=..&p=..&q=..?
+  var b = window.location.search.substr(1).toLowerCase().replace(/[^ctlr]/g,"").trim()
+  if (b == "") b = "clctr"
+  var d = b.replace(/l/g,"R").replace(/r/g,"L").toLowerCase()
+  var p = b.split("").reverse().join("")
+  var q = d.split("").reverse().join("")
+  var hor2x2 = "tile=88,11&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2&patchWidth=10&patchHeight=12&headside=x,7&footside=4,x"
+  var diagonal = "tile=5&shiftColsSW=-1&shiftRowsSW=1&shiftColsSE=1&shiftRowsSE=1&patchWidth=10&patchHeight=12&headside=7,x&footside=x,4"
+  var paris = "tile=B-C-,---5&t&shiftColsSW=-2&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2&patchWidth=12&patchHeight=18"
+  var honeycomb = "tile=-5--,6v9v,---5,2z0z&shiftColsSW=0&shiftRowsSW=4&shiftColsSE=4&shiftRowsSE=4"
+  if (b != d || b!= p) {
+    d3.select(`#diagrams`).append("p").text(`Mirrored stitches: b=${b}, d=${d}, p=${p}, q=${q}. ${ b == p ? "b=p , q=d" : ""} ${ b == d ? "b=d, q=p" : ""}`)
+  }
+
+  showGraph ("diagonal\npair diagram", `b1=${b}&${diagonal}`)
+  showGraph ("paris", `tileStitch=${b}&${paris}`)
+  showGraph ("honeycomb", `tileStitch=${b}&${honeycomb}`)
+  showGraph ("bb ->\nbb <-", `&b1=${b}&b2=${b}&c1=${b}&c2=${b}&${hor2x2}`)
+  if (b != d) {
+    showGraph ("bd ->\nbd <-", `b1=${b}&b2=${d}&c1=${b}&c2=${d}&${hor2x2}`)
+  }
+  if (b != p) {
+    showGraph ("bp ->\nbp <-", `b1=${b}&b2=${p}&c1=${b}&c2=${p}&${hor2x2}`)
+  }
+  if (b != q) {
+    showGraph ("bq ->\nbq <-", `b1=${b}&b2=${q}&c1=${b}&c2=${q}&${hor2x2}`)
+    if (b != d) {
+      showGraph ("bp ->\nqd <-", `b1=${b}&b2=${p}&c1=${q}&c2=${d}&${hor2x2}`)
+    }
+  }
+}
+function showGraph(caption, q) {
+
+  // model
+
+  var config = TilesConfig(q)
+  var diagram = ThreadDiagram.create(NewPairDiagram.create(config))
+  var nodeDefs = diagram.jsNodes()
+  var linkDefs = diagram.jsLinks()//can't inline
+
+  // render
+
+  var scale = 2
+  var height = 180
+  var width = 180
+  var stroke = "2px"
+  var markers = false // use true for pair diagrams on fast devices and other browsers than IE-11
+  var svg = D3jsSVG.render(diagram, stroke, markers, width, height)
+  var fig = d3.select(`#diagrams`).append("figure")
+  var container = fig.append("div")
+  container.html(svg.replace("<g>","<g transform='scale(0.5,0.5)'>"))
+  fig.append("figcaption").append("pre").append("a")
+     .text(caption).attr("href",'tiles?' + q).attr("target", '_blank')
+
+  // nudge nodes with force graph of the  D3js library
+  // TODO the rest of this function is found in other scripts too,
+  //  extract into a single source for more functionality and stay in sync
+
+  var links = container.selectAll(".link").data(linkDefs)
+  var nodes = container.selectAll(".node").data(nodeDefs)
+  function moveNode(jsNode) {
+      return 'translate('+jsNode.x+','+jsNode.y+')'
+  }
+  function drawPath(jsLink) {
+      var s = jsLink.source
+      var t = jsLink.target
+      var l = diagram.link(jsLink.index)
+      return D3jsSVG.pathDescription(l, s.x, s.y, t.x, t.y)
+  }
+  function onTick() {
+      links.attr("d", drawPath);
+      nodes.attr("transform", moveNode);
+  }
+  // read 'weak' as 'invisible'
+  function strength(link){ return link.weak ? link.withPin ? 40 : 10 : 50 }
+  var forceLink = d3
+    .forceLink(linkDefs)
+    .strength(strength)
+    .distance(12)
+    .iterations(30)
+  d3.forceSimulation(nodeDefs)
+    .force("charge", d3.forceManyBody().strength(-1000))
+    .force("link", forceLink)
+    .force("center", d3.forceCenter(width/2, height/2))
+    .alpha(0.0035)
+    .on("tick", onTick)
+}
