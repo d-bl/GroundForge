a8901d7e4fa714b8ad745c51258a89b04718a56f
jo-pol <jo-pol@users.noreply.github.com>
2022-01-07 18:56:30 +0100

    diamond tile for nets page

--- docs/js/nets.js
+++ docs/js/nets.js
@@ -36,7 +36,7 @@ function generate (b, set, colors) {
   const p = b.split("").reverse().join("")
   const q = d.split("").reverse().join("")
   const hor2x2 = "tile=88,11&a1=rctctctctt&l2=lctctctctt&shiftColsSW=0&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2&patchWidth=10&patchHeight=12&headside=x,7&footside=4,x"
-  const diamond = "tile=5-5-,-5-5&shiftColsSW=0&shiftColsSW=-2&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2&patchWidth=12&patchHeight=12&headside=7,x&footside=x,4"
+  const diamond = "tile=-5-,5-5,-5-&headsideStitch=ctctt&shiftColsSW=-2&shiftRowsSW=2&shiftColsSE=2&shiftRowsSE=2&patchWidth=12&patchHeight=12&headside=x,7&footside=4,x"
   const kat = "tile=B-C-,---5,C-B-,-5--,B-C-,---5,C-B-,-5--&shiftColsSW=0&shiftRowsSW=8&shiftColsSE=4&shiftRowsSE=8&patchWidth=17&patchHeight=18&footside=x,4,x,x&headside=x,x,x,7&footsideStitch=ctctctctl&headsideStitch=ctctctctr"
   const weavingKat = "tile=-5---5--,6v9v6v9v,---5---5,2z0z2z0z&headsideStitch=ctct&shiftColsSW=0&shiftRowsSW=4&shiftColsSE=8&shiftRowsSE=4&patchWidth=15&patchHeight=16&footside=rx,r8,x4,11&footside=rx,r8,x4,11&headside=xx,88,7r,1r&footsideStitch=ctct&a2=ctctctctll&headsideStitch=ctct&o4=ctctctctrr"
   d3.select('#b').node().value = b
@@ -97,14 +97,14 @@ function setColors(colors) {
     d3.selectAll('.node').style("opacity","0.2")
     d3.select('#pairs').attr("src","images/dots-legend.png")
 
-    d3.selectAll('.ct-b1, .ct-c4, .ct-d7, .ct-g2').style("fill","#0000FF")
+    d3.selectAll('.ct-b1, .ct-c4, .ct-c3, .ct-d7, .ct-g2').style("fill","#0000FF")
     d3.selectAll('.ct-b2, .ct-d1, .ct-b3, .ct-d5, .ct-b7, .ct-f3, .ct-i4, .ct-i2').style("fill","#FF0000")
 
     d3.selectAll('.ct-c1, .ct-e2, .ct-b5, .ct-j3, .ct-g4').style("fill","#00FFFF")
     d3.selectAll('.ct-c1, .ct-e2, .ct-b5, .ct-j3, .ct-g4').style("opacity","0.25")
 
-    d3.selectAll('.ct-c2, .ct-e4, .ct-d3, .ct-e6, .ct-c8, .ct-h1').style("fill","#00FF00")
-    d3.selectAll('.ct-c2, .ct-e4, .ct-d3, .ct-e6, .ct-c8, .ct-h1').style("opacity","0.3")
+    d3.selectAll('.ct-c2, .ct-e4, .ct-d3, .ct-e6, .ct-c8, .ct-h1, .ct-d2').style("fill","#00FF00")
+    d3.selectAll('.ct-c2, .ct-e4, .ct-d3, .ct-e6, .ct-c8, .ct-h1, .ct-d2').style("opacity","0.3")
   }
   d3.selectAll('.bobbin').style("opacity","1")
 }
