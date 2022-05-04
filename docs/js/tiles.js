cf6592d04b94b8ebc7c89e94c5401af368051c9b
jo-pol <jo-pol@users.noreply.github.com>
2022-05-04 18:00:30 +0200

    scroll

--- docs/js/tiles.js
+++ docs/js/tiles.js
@@ -108,7 +108,7 @@ function showProto() {
 
   return config
 }
-function animate () {
+function animatePairs() {
   var container=d3.select('#pairDiagram')
   nudgePairs(container, 744/1.6, 1052/1.6)
   scrollToIfPossible(container.node(), 744/6, 1052/6)
