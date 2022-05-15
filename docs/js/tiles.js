b776fb32c802cfaa37afe2513c7dcd230cdb73db
jo-pol <jo-pol@users.noreply.github.com>
2022-05-15 07:34:17 +0200

    link to new pair legend

--- docs/js/tiles.js
+++ docs/js/tiles.js
@@ -92,7 +92,8 @@ function showProto() {
   clear2()
   var hrefQ = tesselace(query) + query
   d3.select("#link").node().href = "?" + hrefQ
-  d3.select("#poc").node().href = "poc.html?" + pocRef(query)
+  d3.select("#pairLegend").node().href = "?" + hrefQ
+  d3.select("#poc").node().href = "poc?" + pocRef(query)
   //d3.select("#diagrams .colorCode").html("Sorry, a legend is not (yet?) implemented.")
   d3.select("#threadDiagram").html("")
   d3.select("#pairDiagram").html(PairSvg.render(config, 744, 1052, 1.9))
