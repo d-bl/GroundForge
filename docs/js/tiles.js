67ea297e97b8f7db9fe6653f736c7d141f8b3101
jo-pol <jo-pol@users.noreply.github.com>
2022-09-16 20:59:05 +0200

    A3 if pair diagram exceeds A4

--- docs/js/tiles.js
+++ docs/js/tiles.js
@@ -114,8 +114,8 @@ function showProto(q) {
 function pairRender(config){
 
     if(config.totalRows>35 || config.totalCols>28) {
-        console.log('A2')
-        PairSvg.render(config.getItemMatrix, 1488, 1104, 1.9)
+        console.log('A3')
+        PairSvg.render(config.getItemMatrix, 1052, 1488, 1.9)
     } else {
         console.log('A4')
         PairSvg.render(config.getItemMatrix, 744, 1052, 1.9)
