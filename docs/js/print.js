8cb82e1befd3a3f395bb66182a38a438ca43a292
jo-pol <jo-pol@users.noreply.github.com>
2022-10-05 21:29:41 +0200

    all diagrams on print friendly page

---
+++ docs/js/print.js
@@ -0,0 +1,66 @@
+function load() {
+    var q = window.location.search.substr(1)
+    var cfg = TilesConfig(q)
+
+    // dimensions for an A4
+    var width = 744
+    var height = 1052
+    console.log(`rows=${cfg.totalRows} cols=${cfg.totalCols} -> ${(cfg.totalRows+2)*15},${(cfg.totalCols+2)*15} ; A4=${width},${height}`)
+
+    var zoom = 1.9
+    var itemMatrix = cfg.getItemMatrix
+    var svg = PairSvg.render(itemMatrix, width, height, zoom)
+    d3.select('#enum4perStitch').html(PairSvg.legend(itemMatrix))
+    d3.select('#pair4perStitch').html(svg)
+    d3.select('#pair4perStitchAnimated').html(svg)
+    d3.select('#proto').html(PrototypeDiagram.create(cfg))
+    nudgePairs(d3.select('#pair4perStitchAnimated'))
+    var pairDiagram = NewPairDiagram.create(cfg)
+    var threadDiagram = ThreadDiagram.create(pairDiagram)
+    d3.select('#pair1perStitch').html(DiagramSvg.render(pairDiagram, 2, true, width, height, 0))
+    d3.select('#thread').html(DiagramSvg.render(threadDiagram, 2, true, width, height, 0))
+    var strokeWidth = 2
+    nodeTransparency = 0
+    // TODO centralize
+    showGraph(d3.select('#pair1perStitchAnimated'), pairDiagram, strokeWidth, width, height, 0)
+    showGraph(d3.select('#thread'), threadDiagram, strokeWidth, width, height, nodeTransparency)
+}
+function showGraph(container, diagram, stroke, width, height, transparency) {
+// copied from API/thread.html
+    var markers = true // use false for slow devices and IE-11, set them at onEnd
+    container.html(DiagramSvg.render(diagram, stroke, markers, width, height, transparency))
+
+    var nodeDefs = diagram.jsNodes()
+    var linkDefs = diagram.jsLinks()//can't inline
+    var links = container.selectAll(".link").data(linkDefs)
+    var nodes = container.selectAll(".node").data(nodeDefs)
+    function moveNode(jsNode) {
+        return 'translate('+jsNode.x+','+jsNode.y+')'
+    }
+    function drawPath(jsLink) {
+        var s = jsLink.source
+        var t = jsLink.target
+        var l = diagram.link(jsLink.index)
+        return DiagramSvg.pathDescription(l, s.x, s.y, t.x, t.y)
+    }
+    var tickCounter = 0
+    function onTick() {
+        if (0 !=  (tickCounter++ % 5) ) return
+        links.attr("d", drawPath);
+        nodes.attr("transform", moveNode);
+    }
+    // read 'weak' as 'invisible'
+    function strength(link){ return link.weak ? link.withPin ? 40 : 10 : 50 }
+    var forceLink = d3
+      .forceLink(linkDefs)
+      .strength(strength)
+      .distance(12)
+      .iterations(30)
+    d3.forceSimulation(nodeDefs)
+      .force("charge", d3.forceManyBody().strength(-1000))
+      .force("link", forceLink)
+      .force("center", d3.forceCenter(width/2, height/2))
+      .alpha(0.0035)
+      .on("tick", onTick)
+}
+
