b170adadd705d94f941fd7b18294e5016f809ae9
jo-pol <jo-pol@users.noreply.github.com>
2022-10-09 13:04:36 +0200

    print button / thread colors

--- docs/js/print.js
+++ docs/js/print.js
@@ -2,34 +2,40 @@ function load() {
     var q = window.location.search.substr(1)
     var cfg = TilesConfig(q)
 
-    // dimensions for an A4
-    var width = 744
-    var height = 1052
-    console.log(`rows=${cfg.totalRows} cols=${cfg.totalCols} -> ${(cfg.totalRows+2)*15},${(cfg.totalCols+2)*15} ; A4=${width},${height}`)
+    // dimensions for an A1
+    var width = 2245
+    var height = 3178
 
+    var opacity = 0
     var zoom = 1.9
+    var stroke = 2
     var itemMatrix = cfg.getItemMatrix
     var svg = PairSvg.render(itemMatrix, width, height, zoom)
+    d3.select('#def').attr("href","tiles?"+q)
     d3.select('#enum4perStitch').html(PairSvg.legend(itemMatrix))
     d3.select('#pair4perStitch').html(svg)
     d3.select('#pair4perStitchAnimated').html(svg)
     d3.select('#proto').html(PrototypeDiagram.create(cfg))
-    nudgePairs(d3.select('#pair4perStitchAnimated'), cfg.totalCols/2 * 12, cfg.totalRows/2 * 12)
+    nudgePairs(d3.select('#pair4perStitchAnimated'), cfg.totalCols*6, cfg.totalRows*6)
     var pairDiagram = NewPairDiagram.create(cfg)
     var threadDiagram = ThreadDiagram.create(pairDiagram)
-    d3.select('#pair1perStitch').html(DiagramSvg.render(pairDiagram, 2, true, width, height, 0))
-    d3.select('#thread').html(DiagramSvg.render(threadDiagram, 2, true, width, height, 0))
-    var strokeWidth = 2
-    nodeTransparency = 0
-    // TODO centralize
-    showGraph(d3.select('#pair1perStitchAnimated'), pairDiagram, strokeWidth, width, height, 0)
-    showGraph(d3.select('#thread'), threadDiagram, strokeWidth, width, height, nodeTransparency)
+    d3.select('#pair1perStitch').html(DiagramSvg.render(pairDiagram, stroke, true, width, height, opacity))
+    showGraph(d3.select('#pair1perStitchAnimated'), pairDiagram, stroke, width, height, opacity)
+    showGraph(d3.select('#thread'), threadDiagram, stroke, width, height, opacity)
     d3.select('#thread g').attr("transform","scale(0.5,0.5)")
 }
-function showGraph(container, diagram, stroke, width, height, transparency) {
+function clickedThread(event) {
+    var threadSegments = d3.selectAll("#thread ." + event.currentTarget.textContent.replace(" ",""))
+    var color = d3.select('#threadColor').node().value
+    threadSegments.style("stroke", color)
+    threadSegments.filter(".node").style("fill", color)
+}
+
+function showGraph(container, diagram, stroke, width, height, opacity) {
 // copied from API/thread.html, added moveToNW
+
     var markers = true // use false for slow devices and IE-11, set them at onEnd
-    container.html(DiagramSvg.render(diagram, stroke, markers, width, height, transparency))
+    container.html(DiagramSvg.render(diagram, stroke, markers, width, height, opacity))
 
     var nodeDefs = diagram.jsNodes()
     var linkDefs = diagram.jsLinks()//can't inline
@@ -84,6 +90,6 @@ function showGraph(container, diagram, stroke, width, height, transparency) {
       .force("center", d3.forceCenter(width/2, height/2))
       .alpha(0.0035)
       .on("tick", onTick)
-      .on("tick", moveToNW)
+      .on("end", moveToNW)
 }
 
