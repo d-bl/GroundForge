4038fce22d8ccbc8c34ed5f1f73c8b61ae231b48
jo-pol <jo-pol@users.noreply.github.com>
2022-10-16 16:48:10 +0200

    added whiting reference to print/pdf page

--- docs/js/print.js
+++ docs/js/print.js
@@ -23,6 +23,19 @@ function load() {
     showGraph(d3.select('#pair1perStitchAnimated'), pairDiagram, stroke, width, height, opacity)
     showGraph(d3.select('#thread'), threadDiagram, stroke, width, height, opacity)
     d3.select('#thread g').attr("transform","scale(0.5,0.5)")
+    q.split("&").find(whiting)
+}
+function whiting (kv) {
+    var k = kv.trim().replace(/[^a-zA-Z0-9]/g,"")
+    if (!kv.startsWith("whiting")) return false
+    // side effect: add whiting link
+    var pageNr = kv.split("P")[1]
+    var cellNr = kv.split("_")[0].split("=")[1]
+    d3.select('#whiting').node().innerHTML =
+        "<img src='/gw-lace-to-gf/w/page" + pageNr + "a.gif' title='"+cellNr+"'>"+
+        " Page <a href='http://www.theedkins.co.uk/jo/lace/whiting/page" + pageNr + ".htm'>" + pageNr + "</a> "+
+        "of '<em>A Lace Guide for Makers and Collectors</em>' by Gertrude Whiting; 1920."
+    return true
 }
 function clickedThread(event) {
     var threadSegments = d3.selectAll("#thread ." + event.currentTarget.textContent.replace(" ",""))
