d-bl/GroundForge

commit aeb163da4c2d0d86bfc4dab1a89d00ece5807960

split tiles page into pattern/stitches/droste (#226)
* with new convenience buttons to resize space for diagrams
* print/PDF friendly on their own
* highlight stitches

docs/js/print.js
@@ -1,14 +1,7 @@
 function load() {
     var q = window.location.search.substr(1)
     var cfg = TilesConfig(q)
-
-    // dimensions for an A1
-    var width = 2245
-    var height = 3178
-
-    var opacity = 0
     var zoom = 1.9
-    var stroke = 2
     var itemMatrix = cfg.getItemMatrix
     var svg = PairSvg.render(itemMatrix, width, height, zoom)
     d3.select('#def').attr("href","tiles?"+q)
@@ -16,12 +9,12 @@ function load() {
     d3.select('#pair4perStitch').html(svg)
     d3.select('#pair4perStitchAnimated').html(svg)
     d3.select('#proto').html(PrototypeDiagram.create(cfg))
-    nudgePairs(d3.select('#pair4perStitchAnimated'), cfg.totalCols*6, cfg.totalRows*6)
+    nudgePairs('#pair4perStitchAnimated', cfg.totalCols*6, cfg.totalRows*6)
     var pairDiagram = NewPairDiagram.create(cfg)
     var threadDiagram = ThreadDiagram.create(pairDiagram)
     d3.select('#pair1perStitch').html(DiagramSvg.render(pairDiagram, stroke, true, width, height, opacity))
-    showGraph(d3.select('#pair1perStitchAnimated'), pairDiagram, stroke, width, height, opacity)
-    showGraph(d3.select('#thread'), threadDiagram, stroke, width, height, opacity)
+    showGraph('#pair1perStitchAnimated', pairDiagram)
+    showGraph('#thread', threadDiagram)
     d3.select('#thread g').attr("transform","scale(0.5,0.5)")
     var keyValues = q.split("&")
     keyValues.find(whiting)
@@ -57,63 +50,3 @@ function clickedThread(event) {
     threadSegments.style("stroke", color)
     threadSegments.filter(".node").style("fill", color)
 }
-
-function showGraph(container, diagram, stroke, width, height, opacity) {
-// copied from API/thread.html, added moveToNW
-
-    var markers = true // use false for slow devices and IE-11, set them at onEnd
-    container.html(DiagramSvg.render(diagram, stroke, markers, width, height, opacity))
-
-    var nodeDefs = diagram.jsNodes()
-    var linkDefs = diagram.jsLinks()//can't inline
-    var links = container.selectAll(".link").data(linkDefs)
-    var nodes = container.selectAll(".node").data(nodeDefs)
-    function moveNode(jsNode) {
-        return 'translate('+jsNode.x+','+jsNode.y+')'
-    }
-    function drawPath(jsLink) {
-        var s = jsLink.source
-        var t = jsLink.target
-        var l = diagram.link(jsLink.index)
-        return DiagramSvg.pathDescription(l, s.x, s.y, t.x, t.y)
-    }
-    var tickCounter = 0
-    function onTick() {
-        if (0 !=  (tickCounter++ % 5) ) return
-        links.attr("d", drawPath);
-        nodes.attr("transform", moveNode);
-    }
-
-  function moveToNW() {
-      var x = nodeDefs.reduce(minX).x - 3
-      var y = nodeDefs.reduce(minY).y - 3
-      function moveNode(jsNode) { return 'translate('+(jsNode.x-x)+','+(jsNode.y-y)+')' }
-      function drawPath(jsLink) {
-          var s = jsLink.source
-          var t = jsLink.target
-          var l = diagram.link(jsLink.index)
-          // priority for preventing code duplication over less independency
-          return DiagramSvg.pathDescription(l, s.x - x, s.y-y, t.x - x, t.y -y)
-      }
-      links.attr("d", drawPath);
-      nodes.attr("transform", moveNode);
-  }
-  function minX (min, node) { return min.x < node.x ? min : node }
-  function minY (min, node) { return min.y < node.y ? min : node }
-
-    // read 'weak' as 'invisible'
-    function strength(link){ return link.weak ? link.withPin ? 40 : 10 : 50 }
-    var forceLink = d3
-      .forceLink(linkDefs)
-      .strength(strength)
-      .distance(12)
-      .iterations(30)
-    d3.forceSimulation(nodeDefs)
-      .force("charge", d3.forceManyBody().strength(-1000))
-      .force("link", forceLink)
-      .force("center", d3.forceCenter(width/2, height/2))
-      .alpha(0.0035)
-      .on("tick", onTick)
-      .on("end", moveToNW)
-}
-

docs/API/pair.html
@@ -26,7 +26,7 @@
             d3.select('#legend').html(PairSvg.legend(itemMatrix))
             d3.select('#initial').html(svg)
             d3.select('#animated').html(svg)
-            nudgePairs(d3.select('#animated'), cfg.totalCols/2 * 12, cfg.totalRows/2 * 12)
+            nudgePairs('#animated', cfg.totalCols/2 * 12, cfg.totalRows/2 * 12)
         }
     </script>
 </head>

docs/_layouts/forms.html
@@ -8,6 +8,8 @@
     <meta name="description" content="Choose patterns and stitches to generate diagrams for grounds of bobbin lace">
     <meta name="keywords" content="bobbin lace, pair diagram, thread diagram, patterns, stitches">
     <script src="js/tiles.js"></script>
+    <script src="js/download.js"></script>
+    <script src="js/diagram-figure.js"></script>
     <script src="js/nudgePairs.js"></script>
     <script src="js/d3.v4.min.js" type="text/javascript"></script>
     <link rel="stylesheet" href="css/tiles.css" type="text/css">
@@ -72,7 +74,7 @@
         <figure>
             <figcaption>
                 Pairs
-                <img class="animate" src="images/animate.png" onclick="javascript:nudgePairs(d3.select('#pairDiagram'),125,125)" title="animate/reshape">
+                <img class="animate" src="images/animate.png" onclick="javascript:nudgePairs('#pairDiagram',125,125)" title="animate/reshape">
                 <a href="/GroundForge-help/index#modify-stitches" target="_blank"><img src="images/information-icon.png"></a>
                 <a href="/GroundForge-help/color-rules" target="_blank" title="4 colors per stich rules"><img src="images/to-color-rules.png"></a>
                 <a id="pairLegend" target="_blank" href="legend" title="stitch enumeration"><img src="images/swatches.png"></a>

docs/css/header.css
@@ -1,0 +1,19 @@
+@import url("https://fonts.googleapis.com/css?family=Architects+Daughter");
+header {
+    background: url(../assets/images/header-bg-left.png) top right repeat;
+    font-family: 'Architects Daughter', 'Helvetica Neue', Helvetica, Arial, serif;
+
+}
+header h1 { font-size: 450%; font-weight: 300; margin-top: 0; margin-bottom: 0.2em; letter-spacing: -1px;}
+header h2 { font-size: 163%; font-weight: 300; margin-top: 0; margin-bottom: 0; }
+
+body, header { margin: 0; padding: 0; }
+header div { margin: 0; padding: 1.6em; }
+main, article { margin: 0; padding: 1em; }
+
+header  div {
+    background: url(../assets/images/header-bg-right.png) top right repeat-y;
+}
+header h1 a { color: white; text-decoration: none;}
+header h2 { color: #9ddcff; }
+/*https://developer.mozilla.org/en-US/docs/Learn/HTML/Introduction_to_HTML/Document_and_website_structure*/

docs/css/pattern.css
@@ -1,0 +1,24 @@
+
+figure { max-width:100%; display: inline-block; margin-left: 0; margin-top: 0; margin-right: 0.5em; margin-bottom: 0; }
+figcaption { text-align: center; background-color: #ddd; border-radius: 3px; margin-top: 0.3em; padding-left: 0.5em; padding-right: 0.5em; }
+figcaption img { vertical-align: middle; margin-left: 1em; }
+#prototype {
+  resize:both;
+  overflow:auto;
+  display:inline-block;
+  width:250px; height:250px;
+  max-width:100%; max-height:100%;
+  border: solid 1px  #ddd;
+  margin-bottom: 5em;
+}
+foreignObject { display: none }
+
+input[type="number"] { width:3em; margin-right: 2px; text-align: right; }
+main div { display: inline-block; vertical-align: top; }
+button a { text-decoration: none; color: black; }
+
+#to_self { display: none }
+@media print {
+  form, img, header, figcaption, .pattern svg, td button { display: none }
+  #to_self { display: block }
+}

docs/css/print.css
@@ -1,4 +1,4 @@
-div {
+main div {
     display: inline-block; overflow: hidden; resize: both;
     width: 300px; height: 300px;
     vertical-align: text-top;
@@ -9,4 +9,3 @@ div {
 input { display: none }
 #threadColor, form { display: inline-block }
 form { vertical-align: text-top }
-@media print { form {display: none} }

docs/css/stitches.css
@@ -1,0 +1,29 @@
+
+figure { max-width:90%; display: inline-block; margin-left: 0; margin-top: 0; margin-right: 0.5em; margin-bottom: 0; }
+figcaption { text-align: center; background-color: #ddd; border-radius: 3px; margin-top: 0.3em; padding-left: 0.5em; padding-right: 0.5em; }
+figcaption img { vertical-align: middle; margin-left: 1em; }
+figure { vertical-align: top;}
+
+figure div {
+    resize: both;
+    overflow: auto;
+    display: inline-block;
+    width: 250px; height: 250px;
+    vertical-align: text-top;
+    max-width:100%; max-height:100%;
+    border: solid 1px #DDD;
+}
+div, form { display: inline-block; vertical-align: bottom;}
+main div { min-width: 35%; }
+#whiting {display: none; }
+form div {display: inline-block; vertical-align: top; }
+#paintStitches { width: 6em }
+#threadColor { width: 2.5em }
+div input { width: 3em }
+
+/* the first button on the form (dummy) is activated when typing enter in an input field */
+#to_self, #dummy { display: none }
+@media print {
+    form, header, figcaption { display: none }
+    #to_self { display: block }
+}

docs/droste.html
@@ -1,0 +1,155 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title lang="en">GF: Pair diagrams from thread diagrams</title>
+    <script src="js/d3.v4.min.js" type="text/javascript"></script>
+    <script src="js/GroundForge-opt.js" type="text/javascript"></script>
+    <script src="js/showGraph.js" type="text/javascript"></script>
+    <script src="js/droste.js" type="text/javascript"></script>
+    <script src="js/diagram-figure.js"></script>
+    <link rel="stylesheet" href="css/header.css" type="text/css">
+    <link rel="stylesheet" href="css/stitches.css" type="text/css">
+
+    <link rel="apple-touch-icon" sizes="120x120" href="/GroundForge/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/GroundForge/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/GroundForge/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/GroundForge/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/GroundForge/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <link rel="shortcut icon" href="/GroundForge/favicon/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-config" content="/GroundForge/favicon/browserconfig.xml">
+    <meta name="theme-color" content="#ffffff">
+</head>
+<body onload="load()">
+<header>
+    <div>
+        <h1><a href="/">Diagrams for Bobbin Lace</a></h1>
+        <h2>GroundForge: Pair diagrams from thread diagrams</h2>
+    </div>
+</header>
+<main>
+    <a id="to_self" href="#">Stitches and threads</a>
+    <form onsubmit="return false" action="javascript:void(0);">
+        <button id="dummy" onclick="return false"></button>
+        <p>
+            Back to the initial <a id="to_stitches" href="#"
+        >pair diagram</a>.
+        </p>
+        <p>
+            <label for="threadColor">Click a bobbin or thread start to highlight a thread with</label>
+            <input type="color" id="threadColor" name="threadColor" value="#ff0000">
+            <label for="threadColor">click the button to select another color.</label>
+        </p>
+    </form>
+    <br>
+    <h3>First step</h3>
+    <figure>
+        <figcaption>
+            Stitches
+            <a href="/GroundForge-help/Droste-effect" target="_blank"><img src="images/information-icon.png" title="help"></a>
+            <a href="#" onclick="showDroste(2)" title="regenerate pair diagram and thread diagram"><img alt="wand" src="images/wand.png"></a>
+            <img src="images/maximize.png" onclick="javascript:maximize('#droste2')" title="maximize room for the diagram"/>
+            <img src="images/reset-dimensions.png" onclick="javascript:resetDimensions('#droste2')" title="reset room for the diagram"/>
+            <img src="images/minimize.png" onclick="javascript:minimize('#droste2')" title="minimize room for the diagram"/>
+        </figcaption>
+        <textarea id="droste2" name="droste2" rows="8" cols="30" spellcheck="false" onchange="clear2()" placeholder="Type stitch definition here.
+If nothing is typed, the stitch 'ctc' will be used.
+
+Example:
+
+twist=ct
+cross=ctct"></textarea>
+    </figure>
+    <figure>
+        <figcaption>
+            Pairs
+            <a href="/GroundForge-help/Color-Code#for-the-droste-diagrams" target="_blank"
+            ><img class="swatches" src="images/swatches.png" title="color legend"
+            ></a>
+            <a href="javascript:prepareDownload('#drostePair2')" onfocus="this.href=getDownloadContent('#drostePair2')" onblur="this.href='javascript:prepareDownload(`#drostePair2`)'" download="GF-droste-pair-diagram.svg" title="download" class="download"><img src="images/download.jpg"></a>
+            <img src="images/maximize.png" onclick="maximize('#drostePair2')" title="maximize room for the diagram"/>
+            <img src="images/reset-dimensions.png" onclick="resetDimensions('#drostePair2')" title="reset room for the diagram"/>
+            <img src="images/minimize.png" onclick="minimize('#drostePair2')" title="minimize room for the diagram"/>
+        </figcaption>
+        <div id="drostePair2"></div>
+    </figure>
+    <figure>
+        <figcaption>
+            Threads
+            <a href="javascript:prepareDownload('#drosteThread2')" onfocus="this.href=getDownloadContent('#drosteThread2')" onblur="this.href='javascript:prepareDownload(`#drosteThread2`)'" download="GF-droste-thread-diagram.svg" title="download" class="download"><img src="images/download.jpg"></a>
+            <a id="drosteThread2DownloadLink" download="GF-droste-thread-diagram.svg" href="#?pleasePrepareFirst" onblur="this.href='#?pleasePrepareFirst'" style="display:none" onclick="this.style.display = 'none'">download</a>
+            <img src="images/maximize.png" onclick="javascript:maximize('#drosteThread2')" title="maximize room for the diagram"/>
+            <img src="images/reset-dimensions.png" onclick="javascript:resetDimensions('#drosteThread2')" title="reset room for the diagram"/>
+            <img src="images/minimize.png" onclick="javascript:minimize('#drosteThread2')" title="minimize room for the diagram"/>
+        </figcaption>
+        <div id="drosteThread2"></div>
+    </figure>
+    <h3>Second step</h3>
+    <figure>
+        <figcaption>
+            Stitches
+            <a href="/GroundForge-help/Advanced#thread-diagram-as-pair-diagram" target="_blank"><img src="images/information-icon.png" title="help"></a>
+            <a href="#" onclick="showDroste(3)" title="regenerate pair diagram and thread diagram"><img alt="wand" src="images/wand.png"></a>
+            <img src="images/maximize.png" onclick="javascript:maximize('#droste3')" title="maximize room for the diagram"/>
+            <img src="images/reset-dimensions.png" onclick="javascript:resetDimensions('#droste3')" title="reset room for the diagram"/>
+            <img src="images/minimize.png" onclick="javascript:minimize('#droste3')" title="minimize room for the diagram"/>
+        </figcaption>
+        <textarea id="droste3" name="droste3" rows="8" cols="30" spellcheck="false" onchange="clear3()" placeholder="Type stitch definition here.
+If nothing is typed, the stitch 'ctc' will be used.
+
+Example:
+
+ct
+a140=a150=b240=b250=ctcctct"></textarea>
+    </figure>
+    <figure>
+        <figcaption>
+            Pairs
+            <a href="/GroundForge-help/Color-Code#for-the-droste-diagrams" target="_blank"
+            ><img class="swatches" src="images/swatches.png" title="color legend"
+            ></a>
+            <a href="javascript:prepareDownload('#drostePair3')" onfocus="this.href=getDownloadContent('#drostePair3')" onblur="this.href='javascript:prepareDownload(`#drostePair3`)'" download="GF-droste-next-pair-diagram.svg" title="download" class="download"><img src="images/download.jpg"></a>
+            <a id="drostePair3DownloadLink" download="GF-droste-next-pair-diagram.svg" href="#?pleasePrepareFirst" onblur="this.href='#?pleasePrepareFirst'" style="display:none" onclick="this.style.display = 'none'">download</a>
+            <img src="images/maximize.png" onclick="maximize('#drostePair3')" title="maximize room for the diagram"/>
+            <img src="images/reset-dimensions.png" onclick="resetDimensions('#drostePair3')" title="reset room for the diagram"/>
+            <img src="images/minimize.png" onclick="minimize('#drostePair3')" title="minimize room for the diagram"/>
+        </figcaption>
+        <div id="drostePair3"></div>
+    </figure>
+    <figure>
+        <figcaption>
+            Threads
+            <a href="javascript:prepareDownload('#drosteThread3')" onfocus="this.href=getDownloadContent('#drosteThread3')" onblur="this.href='javascript:prepareDownload(`#drosteThread3`)'" download="GF-droste-next-thread-diagram.svg" title="download" class="download"><img src="images/download.jpg"></a>
+            <a id="drosteThread3DownloadLink" download="GF-droste-net-thread-diagram.svg" href="#?pleasePrepareFirst" onblur="this.href='#?pleasePrepareFirst'" style="display:none" onclick="this.style.display = 'none'">download</a>
+            <img src="images/maximize.png" onclick="javascript:maximize('#drosteThread3')" title="maximize room for the diagram"/>
+            <img src="images/reset-dimensions.png" onclick="javascript:resetDimensions('#drosteThread3')" title="reset room for the diagram"/>
+            <img src="images/minimize.png" onclick="javascript:minimize('#drosteThread3')" title="minimize room for the diagram"/>
+        </figcaption>
+        <div id="drosteThread3"></div>
+    </figure>
+</main>
+<!-- Start of StatCounter Code for Default Guide -->
+<script type="text/javascript">
+    var sc_project=10865294;
+    var sc_invisible=0;
+    var sc_security="2e4f5d96";
+    var scJsHost = (("https:" == document.location.protocol) ?
+        "https://secure." : "http://www.");
+    document.write("<sc"+"ript type='text/javascript' src='" +
+        scJsHost+
+        "statcounter.com/counter/counter.js'></"+"script>");
+</script>
+<noscript>
+    <div class="statcounter"
+    ><a title="web analytics"
+        href="http://statcounter.com/"
+        target="_blank"><img
+            class="statcounter"
+            src="http://c.statcounter.com/10865294/0/2e4f5d96/0/"
+            alt="web analytics"
+    ></a></div>
+</noscript>
+<!-- End of StatCounter Code for Default Guide -->
+</body>
+</html>

commit 033bbe16d6adb1271081733ad31ee24e471d993f

fixed print page broken by #226

docs/js/print.js
@@ -2,7 +2,14 @@ function load() {
     var q = window.location.search.substr(1)
     var cfg = TilesConfig(q)
     var zoom = 1.9
+    var stroke = 2
+    var opacity = 0
     var itemMatrix = cfg.getItemMatrix
+
+    // dimensions for an A4
+    var width = 744
+    var height = 1052
+
     var svg = PairSvg.render(itemMatrix, width, height, zoom)
     d3.select('#def').attr("href","tiles?"+q)
     d3.select('#enum4perStitch').html(PairSvg.legend(itemMatrix))