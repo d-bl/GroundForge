bf3a246faa988c0bcd46547ce75141b60bf8dfa8
jo-pol <jo-pol@users.noreply.github.com>
2023-07-30 17:05:53 +0200

    fork-warning

--- docs/js/fork-warning.js
+++ docs/js/fork-warning.js
@@ -1,4 +1,4 @@
-if (!window.location.host.startsWith(d_dbl)) {
+if (!window.location.host.startsWith('d-dl')) {
     const url = (window.location+'').replace(window.location.host, "d-bl.github.io")
     document.write('<p class=deprecated>This version may be ahead or behind of the <a href="'+url+'">official version</a>.</p>')
 }
