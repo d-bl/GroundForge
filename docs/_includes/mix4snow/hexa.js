91250a553fd10da64b87bf66e7991c45179f23ed
jo-pol <jo-pol@users.noreply.github.com>
2024-12-21 10:06:31 +0100

    consistent twists

--- docs/_includes/mix4snow/hexa.js
+++ docs/_includes/mix4snow/hexa.js
@@ -1,5 +1,5 @@
 const drosteURL = "/GroundForge/droste?source=mix4snow&";
-const stitchesURL = "/GroundForge/stitches?=mix4snow&";
+const stitchesURL = "/GroundForge/stitches?source=mix4snow&";
 
 function setHref(hexaId, stitchesId, drosteHrefId, printHrefId, startId) {
     function parseMatrix(str) {
@@ -93,23 +93,23 @@ function setHref(hexaId, stitchesId, drosteHrefId, printHrefId, startId) {
 
     switch (hexaId) {
         case "hexaCenter":
-            replaceTile(13, 0);
-            replaceStitches(["b1", "c1", "b2", "c2", "b3", "c3", "b4", "c4", "b5", "c5"]);
+            replaceTile(12, 0);
+            replaceStitches(["b16", "c16", "b1", "c1", "b2", "c2", "b3", "c3", "b4", "c4"]);
             break;
         case "hexaNW":
         case "hexaSE":
-            replaceTile(1, 2);
-            replaceStitches(["d5", "e5", "d6", "e6", "d7", "e7", "d8", "e8", "d9", "e9"]);
+            replaceTile(0, 2);
+            replaceStitches(["d4", "e4", "d5", "e5", "d6", "e6", "d7", "e7", "d8", "e8"]);
             break;
         case "hexaN":
         case "hexaS":
-            replaceTile(5, 0);
-            replaceStitches(["b9", "c9", "b10", "c10", "b11", "c11", "b12", "c12", "b13", "c13"]);
+            replaceTile(4, 0);
+            replaceStitches(["b8", "c8", "b9", "c9", "b10", "c10", "b11", "c11", "b12", "c12"]);
             break;
         case "hexaNE":
         case "hexaSW":
-            replaceTile(9, 2);
-            replaceStitches(["d13", "e13", "d14", "e14", "d15", "e15", "d16", "e16", "d1", "e1"]);
+            replaceTile(8, 2);
+            replaceStitches(["d12", "e12", "d13", "e13", "d14", "e14", "d15", "e15", "d16", "e16"]);
             break;
     }
     let newQ = Array
@@ -155,23 +155,21 @@ function diagrams(q) {
     }
     setTimeout(() => {
         nudgePairs('#pairs', cfg.totalCols * 6, cfg.totalRows * 6)
-        d3.selectAll(".bobbin").remove();
         d3.select('#pairs').selectAll(".node").attr("onclick",null)
         d3.select('#pairs').selectAll(".node").on("click",clickedPairStitch)
 
-        paintThreadIntersections(/[bc][1-5][0-9]$/, '#0571b0ff');
-        paintThreadIntersections(/[de][5-9][0-9]$/, '#92c5deff');
-        paintThreadIntersections(/[bc](9|(1[0-3]))[0-9]$/, '#ca0020ff');
-        paintThreadIntersections(/[de](1|(1[3-6]))[0-9]$/, '#f4a582ff');
+        paintThreadIntersections(/[bc]([1-4]|(16))[0-9]$/, '#0571b0ff');
+        paintThreadIntersections(/[de][4-8][0-9]$/, '#92c5deff');
+        paintThreadIntersections(/[bc]([89]|(1[0-2]))[0-9]$/, '#ca0020ff');
+        paintThreadIntersections(/[de](1[2-6])[0-9]$/, '#f4a582ff');
     }, 0);
 }
 
 function recipe(stitches,startsLeft) {
+    // used as link  in a markdown containing the form
+    // javascript:recipe("ctc,...",true/false)
     document.getElementById('replacement').value = stitches
-    if (startsLeft)
-        document.getElementById('left').checked = true
-    else
-        document.getElementById('right').checked = true
+    document.getElementById(startsLeft?'left':'right').checked = true
 }
 
 
@@ -210,7 +208,8 @@ function flip_b2p(id) {
 document.addEventListener('DOMContentLoaded', (event) => {
     var q = document.URL.split(/[?#]/)[1];
     if (!q) {
-        q = "patchWidth=12&patchHeight=35&footside=r,x,x,x,x,4,r,r&tile=31rx,17x-,rxx-,rxx-,rx83,-w48,-xxr,-xxr,31xr,17-x,rx-x,rx-x,rx31,-w17,-xrx,-xrx,&headside=8,r,r,r,r,x,x,x&shiftColsSW=0&shiftRowsSW=16&shiftColsSE=4&shiftRowsSE=8&d1=rc&c1=tc&b1=lcrclc&a1=rrctt&c2=crclcr&n5=llctt&d5=cr&b5=ct&e9=lc&c9=cr&e13=cl&b13=lc";
+        q = "patchWidth=14" +
+            "&patchHeight=35&footside=x,x,x,x,4,r,r,r&tile=31rx,17x-,rxx-,rxx-,rx83,-w48,-xxr,-xxr,31xr,17-x,rx-x,rx-x,rx31,-w17,-xrx,-xrx,&headside=x,x,x,8,r,r,r,r&shiftColsSW=0&shiftRowsSW=16&shiftColsSE=4&shiftRowsSE=8&d1=rc&c1=tc&b1=lcrclc&a1=rrctt&c2=crclcr&n5=llctt&d5=cr&b5=ct&e9=lc&c9=cr&e13=cl&b13=lc";
     }
     document.getElementById('toDiagrams').setAttribute("href", drosteURL + q);
     document.getElementById('toPrintFriendly').setAttribute("href", stitchesURL + q);
