058bd2752ef952eef83fc29edcda57b025576d06
jo-pol <jo-pol@users.noreply.github.com>
2024-12-23 21:45:58 +0100

    a twist too many

--- docs/_includes/mix4snow/hexa.js
+++ docs/_includes/mix4snow/hexa.js
@@ -214,7 +214,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             "&shiftColsSW=0&shiftRowsSW=16&shiftColsSE=4&shiftRowsSE=8&patchWidth=14&patchHeight=35" +
             "&i1=rc&h1=ct&g1=clcrcl&h2=crclcr&n5=llctt&i5=ctc&g5=ct&j9=lc&h9=ctc&j13=ctc&g13=lc&g16=tc&h16=rclcrc&h4=ct&h8=cr&g8=ctc&g9=lc&g12=lc&i12=ctc&j12=cl&i13=rc&j16=lc&j4=ctc&i4=cr&j5=lc&i8=rc&j8=lc&i16=rc" +
             "&droste2=g160=g161=h160=ttctc,g15=h41=h42=ctctt" +
-            ",,g80=j120=j121=lllctc,g81=llllctcl,h80=rrrctc,h81=rrrctcr,g120=g121=ctclll,h92=h93=i160=i161=ctcrrr" +
+            ",,g80=j120=j121=lllctc,g81=lllctcl,h80=rrrctc,h81=rrrctcr,g120=g121=ctclll,h92=h93=i160=i161=ctcrrr" +
             ",j121=lllctcl,j133=ctcl,i53=i41=ctcr" +
             ",,f80=p81=ctcttttttttttttlllctc,f82=rrtctctrr"+
             ",,u82=ctc,u81=tttttttctctttttt,u83=tttttctc,u80=tttctc";
