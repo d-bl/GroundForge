f6617dcb83bd14ad7a4631fc425bb81cd69e8f08
jo-pol <jo-pol@users.noreply.github.com>
2025-10-29 14:33:07 +0100

    css out of sync

---
+++ compare-with-help.sh
@@ -0,0 +1,5 @@
+#diff -y docs/_layouts/default.html ../GroundForge-help/docs/_layouts/default.html
+diff -y docs/_includes/Sidebar-default.md ../GroundForge-help/docs/_includes/Sidebar.md
+
+echo '####################################### docs/assets/css/help.css ######'
+diff -y docs/assets/css/help.css ../GroundForge-help/docs/assets/css/help.css
